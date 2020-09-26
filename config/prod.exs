5483bbce2aa650542b88f3139877c4834495f043
Patrik Rendek <patrik.rendek2@gmail.com>
2020-09-26 19:41:27 +0200

    prepares for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE")
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
