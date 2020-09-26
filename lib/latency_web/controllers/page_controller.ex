52424947e60ca280713952597d0a9abae674f7b5
Patrik Rendek <patrik.rendek2@gmail.com>
2020-09-26 15:58:53 +0200

    Comments added, parsing status code changed

--- lib/latency_web/controllers/page_controller.ex
+++ lib/latency_web/controllers/page_controller.ex
@@ -2,28 +2,47 @@ defmodule LatencyWeb.PageController do
   use LatencyWeb, :controller
   alias Latency.Response
   alias Latency.Repo
-  import Plug.Conn
 
+  @doc """
+  Method for rendering index.html.
+  """
 
-
-  def index(conn,params) do
-    render(conn, "index.html",url: params["url"])
+  def index(conn,_params) do
+    render(conn, "index.html")
   end
 
+  @doc """
+  Method for parsing response code,returns 500 if an error occurs, if does not returns status code.
+  """
+  def get_response_code(url) do
+    case HTTPoison.get(url) do
+      {:ok,%HTTPoison.Response{status_code: status_code}} ->
+        status_code
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        500
+    end 
+  end
+
+  @doc """
+  Method for getting latency,status_code and time when a request was sent. Save response object to db,
+  returns json representation of response object, if an error occurs it returns json with "Failed".
+
+  """
   def check_response(conn,params) do
+    status=0
     url=params["url"]
     time_now=DateTime.utc_now
     start_ms = System.monotonic_time(:milliseconds)
-     # do the request and receive response
-    response_json = HTTPoison.get!(url)
+    # do the request and receive response
+    status=get_response_code(url)
     end_ms = System.monotonic_time(:milliseconds)
-    status = response_json.status_code
     latency = end_ms - start_ms
     changeset= Response.changeset(%Response{}, %{latency: latency, status: status,url: url, time: time_now,})
+    # save response obj
     case Repo.insert(changeset) do
-      {:ok, changeset} ->
+      {:ok, _changeset} ->
         json(conn,%{latency: latency, status: status,url: url, time: time_now,})
-      {:error, changeset} ->
+      {:error, _changeset} ->
         json(conn,{"Failed"})
       end
   end
