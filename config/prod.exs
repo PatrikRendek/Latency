337d904d9ef58ac91b0c377f2d492c5c3bb6ef56
Patrik Rendek <patrik.rendek2@gmail.com>
2020-09-26 18:00:18 +0200

    prepares for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :latency, LatencyWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "nameless-eyrie-58895.herokuapp.com.com", port: 443],
+  url: [scheme: "https", host: "https://latency-web-app.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
